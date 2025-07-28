45afc50760d50d32e988e9c686c0217fd46ed735
mattblackford <mzb@phoebussoftware.com>
2025-07-28 10:47:44 +0100

    Add split keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,6 +26,7 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<leader>vS', ':vsplit<CR>', { desc = 'Create [v]ertical [S]plit' })
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
